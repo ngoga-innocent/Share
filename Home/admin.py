f08d7e946eaca74ae7f00dc17f68e753503b3d39
Ngoga Innocent Patrick <ngogainnocent1@gmail.com>
2025-04-29 14:55:59 +0200

    commiting

--- Home/admin.py
+++ Home/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Carousel,Blog,Mission
 # Register your models here.
 admin.site.register(Carousel)
-admin.site.register(Mission)
+# admin.site.register(Mission)
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display=('title',)
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date', 'slug','is_featured')
--- ShareHelp/settings.py
+++ ShareHelp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-72e_a=-(yfg-muv%##h4bhdy78ls9uosjj%t5dy)g&v5qnm5u='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
